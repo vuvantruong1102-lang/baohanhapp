05e3e17c66482b97fb42077b06c961c58bd8e3db
vuvantruong1102-lang <vuvantruong.1102@gmail.com>
2026-05-30 16:33:11 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import WarrantyPage from './pages/WarrantyPage.jsx'
-import AdminPage from './pages/AdminPage.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -12,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Navigate to="/tamayoko" replace />} />
         <Route path="/admin" element={<Dashboard />} />
-        <Route path="/admin/import" element={<AdminPage />} />
         <Route path="/:brand" element={<WarrantyPage />} />
       </Routes>
     </BrowserRouter>

e1b8692aefe32da66d0772f1977bfd8c12d25cbc
vuvantruong1102-lang <vuvantruong.1102@gmail.com>
2026-05-30 16:19:02 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,16 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import WarrantyPage from './pages/WarrantyPage.jsx'
 import AdminPage from './pages/AdminPage.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/tamayoko" replace />} />
-        <Route path="/:brand" element={<WarrantyPage />} />
+        <Route path="/admin" element={<Dashboard />} />
         <Route path="/admin/import" element={<AdminPage />} />
+        <Route path="/:brand" element={<WarrantyPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
